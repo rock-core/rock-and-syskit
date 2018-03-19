c3e817ff577c880c4c99723d944f5b3d770a6ede
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-03-19 10:44:49 -0300

    move the aruba stuff under a rock_website-dev folder alongside the checkout

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -1,1 +1,6 @@
 require 'aruba/cucumber'
+Aruba.configure do |config|
+    checkout_name = File.basename(config.root_directory)
+    config.root_directory = File.expand_path("../", config.root_directory)
+    config.working_directory = "#{checkout_name}-dev"
+end
