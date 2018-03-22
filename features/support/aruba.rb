e2f48dfc17701563d4eb923e902fcd9d2b2ecd42
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2018-03-22 16:58:23 -0300

    features: fix state commit after the Installation

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -7,22 +7,27 @@ Aruba.configure do |config|
     config.working_directory = "#{checkout_name}-dev"
 end
 
+def save_state_as_git_tag(tag_name, path)
+    git_dir = File.join(path, ".git")
+    if !File.directory?(git_dir)
+        if !system("git init", chdir: path, out: :close)
+            raise "Failed to create the git repository in #{path}"
+        end
+    end
+    if !system("git add .", chdir: path, out: :close) || !system("git commit --allow-empty -m '#{tag_name}'", chdir: path, out: :close)
+        raise "Failed to commit the current state"
+    end
+    if !system("git tag -f '#{tag_name}'", chdir: path, out: :close)
+        raise "Failed to create the git repository in #{git_dir}"
+    end
+end
+
 After do |scenario|
     if scenario.passed?
-        bundle_path = File.join(aruba.config.root_directory, aruba.config.working_directory, "dev", "bundles", "syskit_basics")
-        git_dir = File.join(bundle_path, ".git")
-        if File.directory?(bundle_path) && !File.directory?(git_dir)
-            if !system("git init", chdir: bundle_path, out: :close)
-                raise "Failed to create the git repository in #{bundle_path}"
-            end
-        end
-
         tag_name = "#{scenario.feature.name} - #{scenario.name}".gsub(/\s/, '_')
-        if !system("git add .", chdir: bundle_path, out: :close) || !system("git commit --allow-empty -m '#{tag_name}'", chdir: bundle_path, out: :close)
-            raise "Failed to commit the current state"
-        end
-        if !system("git tag -f '#{tag_name}'", chdir: bundle_path, out: :close)
-            raise "Failed to create the git repository in #{git_dir}"
+        bundle_path = File.join(aruba.config.root_directory, aruba.config.working_directory, "dev", "bundles", "syskit_basics")
+        if File.directory?(bundle_path)
+            save_state_as_git_tag(tag_name, bundle_path)
         end
     end
 end
